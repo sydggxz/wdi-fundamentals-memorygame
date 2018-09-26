18a7229f2e661da91280cfef1de852ea2ca7bb9d
GLIC\sydggxz <guangjie_zhang@glic.com>
2018-09-26 01:15:37 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,21 +22,47 @@ var cards = [
     }
 ];
 var cardsInPlay = [];
+var score = 0;
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
+        score++;
+        refreshScore();
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
     }
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+    let cardId = this.getAttribute('data-id');
     cardsInPlay.push(cards[cardId].rank);
     console.log(`User flipped ${cards[cardId].rank}`);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+    for (let i = 0; i< cards.length; i++) {
+        const cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.querySelector('#game-board').appendChild(cardElement);
+    }
+}
+var resetBoard = function() {
+    let gameBoard = document.querySelector('#game-board');
+    while (gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild);
+    }
+    cardsInPlay = [];
+    createBoard();
+}
+var refreshScore = function() {
+    document.querySelector('#game-score').textContent = score;
+}
+document.querySelector('#reset-board').addEventListener('click', resetBoard);
+createBoard();
+refreshScore();
